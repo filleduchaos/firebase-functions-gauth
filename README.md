7bf982ed318f5674bacabdd04031cec38cd89821
PJ Essien <retrostereoyak@gmail.com>
2018-03-27 12:15:15 +0100

    First draft

--- README.md
+++ README.md
@@ -2,3 +2,40 @@
 
 Proof-of-concept of getting offline access to various Google API scopes for a
 serverless Firebase project.
+
+## How to set up
+
+You will need to have created a Firebase account and project, installed the
+Firebase CLI and logged in from the command line.
+
+- Clone the project and change into the project directory
+- Edit the `.firebaserc` file to point the default project to your Firebase
+  project.
+- This demo uses anonymous authentication to capture users, so you'll have to
+  enable that for your project (from the [Firebase console](https://console.firebase.google.com)).
+  Alternatively, you can change to and enable whatever sign-in you want.
+- You will need to obtain your Google project's credentials and register your
+  OAuth callback.
+  - Go to your [Google console](https://console.developers.google.com).
+  - Make sure your Firebase project is the current selected project.
+  - From the `APIs and Services` side menu, navigate to the Credentials page.
+  - Under the `OAuth 2.0 client IDs` section, click on the web client that was
+    auto created by Google.
+  - Add the client ID and secret at the top of the page to your configuration
+    with these commands:
+
+    ```bash
+    firebase functions:config:set gauth.client_id=YOUR_CLIENT_ID
+    firebase functions:config:set gauth.client_secret=YOUR_CLIENT_SECRET
+    ```
+
+  - On the Google Developers' Console page, go to the Authorized Redirect URIs
+    section and add `https://<PROJECT_ID>.firebaseapp.com/google-oauth2-callback`.
+    - If you have your own domain connected to the project, don't forget to add
+      `https://<YOUR_DOMAIN>/google-oauth2-callback` as well.
+  - Save your changes
+    - Optionally, you can go to the OAuth consent screen tab to customize the
+      authorization screen that will be presented to users
+  - From the command line, run `firebase deploy` to push the app live
+
+That's it! :)
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,78 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const { google } = require('googleapis');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+const gauthKeys = functions.config().gauth;
+const googleScopes = [
+  'https://www.googleapis.com/auth/calendar'
+  // Add more scopes if you want! The full scope list is available at
+  // https://developers.google.com/identity/protocols/googlescopes
+];
+
+const OAuth2 = google.auth.OAuth2;
+const oauth2Client = new OAuth2(
+  gauthKeys.client_id,
+  gauthKeys.client_secret,
+  // You can replace the domain here with a custom domain name if you have one
+  `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com/google-oauth2-callback`
+);
+
+// Change the database reference as you like. Don't forget to edit the database
+// rules as well!
+const tokenStore = admin.database().ref('__tokens__');
+
+// Check that user is signed in
+const checkAuthMiddleware = (req, res, next) => {
+  const { idToken } = req.query;
+
+  if (typeof idToken === 'string') {
+    return admin.auth().verifyIdToken(idToken)
+      .then((decodedIdToken) => {
+        // If you want to store tokens under another key, retrieve and set it here
+        req.userID = decodedIdToken.uid;
+        return next();
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(401);
+      });
+  }
+  return res.statusCode(401);
+};
+
+exports.grantGooglePermissions = functions.https.onRequest((req, res) => (
+  checkAuthMiddleware(req, res, () => {
+    const url = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      // This will be passed to the OAuth callback 
+      state: req.userID,
+      // Remove this if you only want access to the user's profile and email
+      scope: googleScopes,
+    });
+  
+    return res.redirect(url);
+  })
+));
+
+exports.googleOAuth2Callback = functions.https.onRequest((req, res) => {
+  const { code, state: userID } = req.query;
+
+  if (code && userID) {
+    return oauth2Client.getToken(code)
+      .then(({ tokens }) => {
+        if (tokens && tokens.refresh_token) {
+          return tokenStore.child(userID).set(tokens.refresh_token)
+            .then(() => {
+              console.log(`Got token for ${userID}`);
+              return res.redirect('/success.html');
+            });
+        }
+
+        return console.error(`Refresh token not found. No action taken.`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+});
